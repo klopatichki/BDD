63174a39d9b0ce1e9e771adb99e5d6308a0b0a2e
Kristijan Lopatichki <kristijan.lopatichki@epfl.ch>
2020-12-13 12:59:19 +0000

    Extract AND and or with caching.

--- src/BDD.hpp
+++ src/BDD.hpp
@@ -101,6 +101,113 @@ private:
         return signal & COMPLEMENT_MASK;
     }
 
+    inline static signal_t cached_computation(
+            std::unordered_map<std::tuple<signal_t, signal_t>, signal_t> &cache,
+            signal_t f,
+            signal_t g,
+            const std::function<signal_t(signal_t, signal_t)>& func) {
+        auto original_order = std::tuple<signal_t, signal_t>(f, g);
+        auto commuted_order = std::tuple<signal_t, signal_t>(g, f);
+        if (cache.find(original_order) != cache.end()) {
+            return cache.find(original_order)->second;
+        } else if (cache.find(commuted_order) != cache.end()) {
+            return cache.find(commuted_order)->second;
+        }
+        return func(f, g);
+    }
+
+    signal_t AND_INTERNAL(signal_t f, signal_t g) {
+        Node const &F = get_node(f);
+        Node const &G = get_node(g);
+
+        /* trivial cases */
+        if (f == constant(false) || g == constant(false)) {
+            return constant(false);
+        }
+        if (f == constant(true)) {
+            return g;
+        }
+        if (g == constant(true)) {
+            return f;
+        }
+        if (f == g) {
+            return f;
+        }
+
+        var_t x;
+        signal_t f0, f1, g0, g1;
+        if (F.v < G.v) /* F is on top of G */
+        {
+            x = F.v;
+            f0 = F.E;
+            f1 = F.T;
+            g0 = g1 = g;
+        } else if (G.v < F.v) /* G is on top of F */
+        {
+            x = G.v;
+            f0 = f1 = f;
+            g0 = G.E;
+            g1 = G.T;
+        } else /* F and G are at the same level */
+        {
+            x = F.v;
+            f0 = F.E;
+            f1 = F.T;
+            g0 = G.E;
+            g1 = G.T;
+        }
+
+        signal_t const r0 = AND(f0, g0);
+        signal_t const r1 = AND(f1, g1);
+        return unique(x, r1, r0);
+    }
+
+    signal_t OR_INTERNAL(signal_t f, signal_t g) {
+        Node const &F = get_node(f);
+        Node const &G = get_node(g);
+
+        /* trivial cases */
+        if (f == constant(true) || g == constant(true)) {
+            return constant(true);
+        }
+        if (f == constant(false)) {
+            return g;
+        }
+        if (g == constant(false)) {
+            return f;
+        }
+        if (f == g) {
+            return f;
+        }
+
+        var_t x;
+        signal_t f0, f1, g0, g1;
+        if (F.v < G.v) /* F is on top of G */
+        {
+            x = F.v;
+            f0 = F.E;
+            f1 = F.T;
+            g0 = g1 = g;
+        } else if (G.v < F.v) /* G is on top of F */
+        {
+            x = G.v;
+            f0 = f1 = f;
+            g0 = G.E;
+            g1 = G.T;
+        } else /* F and G are at the same level */
+        {
+            x = F.v;
+            f0 = F.E;
+            f1 = F.T;
+            g0 = G.E;
+            g1 = G.T;
+        }
+
+        signal_t const r0 = OR(f0, g0);
+        signal_t const r1 = OR(f1, g1);
+        return unique(x, r1, r0);
+    }
+
 public:
     explicit BDD(uint32_t num_vars)
             : unique_table(num_vars), num_invoke_and(0u), num_invoke_or(0u),
@@ -180,6 +287,7 @@ public:
 
     /* Compute ~f */
     signal_t NOT(signal_t f) {
+        // increase count
         return f ^ COMPLEMENT_MASK;
     }
 
@@ -240,97 +348,17 @@ public:
     /* Compute f & g */
     signal_t AND(signal_t f, signal_t g) {
         ++num_invoke_and;
-        Node const &F = get_node(f);
-        Node const &G = get_node(g);
-
-        /* trivial cases */
-        if (f == constant(false) || g == constant(false)) {
-            return constant(false);
-        }
-        if (f == constant(true)) {
-            return g;
-        }
-        if (g == constant(true)) {
-            return f;
-        }
-        if (f == g) {
-            return f;
-        }
-
-        var_t x;
-        signal_t f0, f1, g0, g1;
-        if (F.v < G.v) /* F is on top of G */
-        {
-            x = F.v;
-            f0 = F.E;
-            f1 = F.T;
-            g0 = g1 = g;
-        } else if (G.v < F.v) /* G is on top of F */
-        {
-            x = G.v;
-            f0 = f1 = f;
-            g0 = G.E;
-            g1 = G.T;
-        } else /* F and G are at the same level */
-        {
-            x = F.v;
-            f0 = F.E;
-            f1 = F.T;
-            g0 = G.E;
-            g1 = G.T;
-        }
-
-        signal_t const r0 = AND(f0, g0);
-        signal_t const r1 = AND(f1, g1);
-        return unique(x, r1, r0);
+        return cached_computation(computed_table_AND, f, g, [this] (signal_t f, signal_t g) -> signal_t {
+            return AND_INTERNAL(f, g);
+        });
     }
 
     /* Compute f | g */
     signal_t OR(signal_t f, signal_t g) {
         ++num_invoke_or;
-        Node const &F = get_node(f);
-        Node const &G = get_node(g);
-
-        /* trivial cases */
-        if (f == constant(true) || g == constant(true)) {
-            return constant(true);
-        }
-        if (f == constant(false)) {
-            return g;
-        }
-        if (g == constant(false)) {
-            return f;
-        }
-        if (f == g) {
-            return f;
-        }
-
-        var_t x;
-        signal_t f0, f1, g0, g1;
-        if (F.v < G.v) /* F is on top of G */
-        {
-            x = F.v;
-            f0 = F.E;
-            f1 = F.T;
-            g0 = g1 = g;
-        } else if (G.v < F.v) /* G is on top of F */
-        {
-            x = G.v;
-            f0 = f1 = f;
-            g0 = G.E;
-            g1 = G.T;
-        } else /* F and G are at the same level */
-        {
-            x = F.v;
-            f0 = F.E;
-            f1 = F.T;
-            g0 = G.E;
-            g1 = G.T;
-        }
-
-        signal_t const r0 = OR(f0, g0);
-        signal_t const r1 = OR(f1, g1);
-        return unique(x, r1, r0);
+        return cached_computation(computed_table_OR, f, g, [this] (signal_t f, signal_t g) -> signal_t {
+            return OR_INTERNAL(f, g);
+        });
     }
 
     /* Compute ITE(f, g, h), i.e., f ? g : h */
